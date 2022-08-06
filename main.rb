7304c66804ff04d49a0aaa2f2733103f91e221c7
Jeff Dwyer <jdwyah@gmail.com>
2022-08-06 17:24:31 +0000

    stuff

--- main.rb
+++ main.rb
@@ -1,19 +1,27 @@
+require 'rainbow/refinement'
+using Rainbow
 require './planet_items.rb'
 require './planet.rb'
 
 # TODO
 # pirate raids while in space
-# each planet buys only some things
-# coffee
-# milk
-# chocolate_milk
-# pickles
-
+# battle chance
+## basic lasers  1
+## basic shields 1
+## shoot lasers. compare lasers.  laser 3 vs shields 1. they take your stuff.
+## celestial cow beings
+## run away. send you back.
+# #change flight cost to by gas minus one
+# do all planets sell gas?
 
 @money = 2000
 @running = true
 @current_planet = :mars
 @inventory = {}
+
+@hull_strength = 100
+@shield_level = 1
+@lazer_level = 0
 
 def print_market
   current_planet_obj.market[:supply].each do |key, item|
@@ -32,6 +40,9 @@ end
 
 def status
   puts "You are on planet #{current_planet_obj.name}"
+  puts '====Shippie===='
+  puts 'Your ship is... uh... well it can stay airborne...    mostly'
+  puts "shield level: #{@shield_level} pew pew level: #{@lazer_level}"
   puts "You have #{@money} money"
   puts '====Inventory===='
   print_inventory
@@ -41,9 +52,9 @@ end
 
 def sell_something
   print_inventory
-  puts  "What do you want to sell?"
+  puts 'What do you want to sell?'
   what = gets.chomp.to_sym
-  if @inventory[what].nil? || @inventory[what] <=0
+  if @inventory[what].nil? || @inventory[what] <= 0
     puts "Uh.. you don't have any #{what}"
     return
   end
@@ -93,6 +104,109 @@ def buy_something
   end
 end
 
+def pew_pew_battle
+  # generate bad guy
+
+  bad_guys = [{
+    hull_strength: 100,
+    lasers: 3,
+    shields: 3,
+    name: 'Frankie',
+    description: 'frankie doodle',
+    battle_cries: ['Frankie doodle went to town | and blew up your space ship.'],
+    retreat_cry: 'Frankie doodle ran away ay | from your viscious space ship.'
+  }, {
+    hull_strength: 100,
+    lasers: 3,
+    shields: 3,
+    name: 'Bob',
+    description: 'Bob the genaric guy',
+    battle_cries: [''],
+    retreat_cry: ''
+  }, {
+    hull_strength: 100,
+    lasers: 6,
+    shields: 1,
+    name: 'Flinston',
+    description: "Flinston McFlipperface: The lazer lovin' duck an ecsapee from Farmer Browns' farm who ran off to, in his words \"Make things go BOOM!!!!\"  ",
+    battle_cries: ['Quack quack quack (then maniacle laughter'],
+    retreat_cry: 'Quack quack (what it means: why would I understand duck? YOU go learn duck if you want to know).'
+  }, {
+
+    hull_strength: 1000,
+    lasers: 0,
+    shields: 18,
+    name: 'Floyd',
+    description: 'Floyd the verrrrrrrrrrrrrry paranoid hairdresser a.k.a. a barber gone bad',
+    battle_cries: ['Snip snip snip!', ''],
+    retreat_cry: 'You win this round, but your haircut is still ugly.',
+    cargo: [100, :star_juice]
+  }]
+  bad_guy = bad_guys.sample
+  # tell player the bad guys are there
+
+  puts "🛸"  
+  puts "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
+  puts "All yu wanted to do was zoom. YOU didn't want to b'ow things up (I hope). But the Celestial Cow Beings had other ideas."
+  puts "#{@name} meet #{bad_guy[:description]}. You may now commence killing and pilaging each other."
+
+
+
+  # ask user if they want to run or fight
+  puts "Do you want to [run, fight]?"
+  input = gets.chomp
+
+  if input == 'fight'
+    fight(bad_guy)
+  
+  elsif input == 'run'
+    puts "Run away!!"
+    #if r'n set dest'nation back to where they were
+
+  else
+    puts "???" 
+  end
+  
+end
+
+def fight(bad_guy)
+  # have ba guys say something 
+  puts "#{bad_guy[:name]} says #{bad_guy[:battle_cries].sample}"
+
+
+    # if no cow goddesses
+    
+  #   bad_guy_shields = 2
+  #   bad_guy_lasers = 2
+  our_lasers = 1
+
+  puts "#{@lazer_level.to_f} #{bad_guy[:shields].to_f}"
+ 
+  we_hit = (100 * (@lazer_level.to_f / bad_guy[:shields].to_f)).to_i
+  bad_guy[:hull_strength] -= we_hit
+
+  puts "Lasers fl y!  You do #{we_hit} damage."
+  
+  they_hit = (100 * (bad_guy[:lasers].to_f / @shield_level.to_f)).to_i
+  @hull_strength -= they_hit
+
+  puts "Lasers fl y!  They do #{they_hit} damage."
+
+  
+    
+#  we_hit_them = 100* our_lasers / bad_guy_shields # = 50 hits
+  #   they_hit_us = 100 * their /our shiels = #200 hits 
+
+  #   health_reactor_core_bar = 1000
+  
+#   round 1:  we hit either 50 or 0 if we miss
+  #   round 1:  they hit either 200 or 0 if we miss
+ # i we get to half health... they take all the cargo. 
+  
+  # do you want to run aw
+
+end
+
 def zoom_zoom
   destinations = PLANETS
   puts 'You can fly to:'
@@ -101,16 +215,25 @@ def zoom_zoom
   end
   puts "Where to? [#{current_planet_obj.destinations.map(&:to_s).flatten.join(", ")}]"
   dest = gets.chomp
-
+  pew_pew_battle
+  
   flight_cost = 500
 
   if @money < flight_cost
     puts "Not enough money for the flight! Sorry, it costs #{flight_cost} and you only have #{@money}"
     return
   end
-  puts "You flew to #{dest} for $#{flight_cost}"
   @money -= flight_cost
-  @current_planet = dest.to_sym
+@current_planet = dest.to_sym
+  
+  puts "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
+  puts "You flew to #{dest} for $#{flight_cost}"
+  current_planet_obj.print_welcome
+  puts "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀'🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
+end
+
+def underline_first(word, count =1)
+  word.slice(0,count).underline + word[count..-1]
 end
 
 def main
@@ -123,19 +246,19 @@ def main
 
   @wrong_attempts = 0
   while @running
-    puts 'What would you like to do? [status,buy,sell, zoom, exit]'
+    puts "What would you like to do? [#{underline_first('info')},#{underline_first('buy')},#{underline_first('sell')}, #{underline_first('zoom')}, exit]"
     input = gets.chomp
 
     case input
-    when 'status'
+    when 'info', 'i'
       status
     when 'exit'
       @running = false
-    when 'buy'
+    when 'buy', 'b'
       buy_something
-    when 'zoom'
-      zoom_zoom
-     when 'sell'
+    when 'zoom', 'z'
+     zoom_zoom
+     when 'sell', 's'
       sell_something
     else
       @wrong_attempts += 1
--- planet.rb
+++ planet.rb
@@ -1,5 +1,6 @@
 class Planet
   attr_reader :name, :market, :destinations
+  attr_accessor :description
 
   def initialize(name:, destinations:)
     @name = name
@@ -17,6 +18,11 @@ class Planet
                                        price: price,
                                        quantity: quantity)
   end
+
+  def print_welcome
+    puts "Welcome to #{@name}"
+    puts @description unless @description.nil?
+  end
 end
 
 
@@ -31,10 +37,11 @@ PLANETS[:mars].add_supply(:pickle, 100, 20)
 PLANETS[:mars].add_supply(:coffee, 50, 20)
 PLANETS[:mars].add_supply(:bread, 50, 20)
 
+PLANETS[:mars].add_demand(:mangos, 90, 20)
 PLANETS[:mars].add_demand(:peanut_butter, 100, 20)
-PLANETS[:mars].add_demand(:purple_cow_ice_cream, 100, 20)
-PLANETS[:mars].add_demand(:star_juice, 100, 20)
-
+PLANETS[:mars].add_demand(:puirple_cow_ice_cream, 400, 20)
+PLANETS[:mars].add_demand(:star_juice, 800, 20)
+PLANETS[:mars].description = "Mars is a a dusty red place. Still, you have to check out the Marsket. Get it!? Mars-ket. Hah!"
 
 PLANETS[:venus].add_supply(:cheese, 100, 20)
 PLANETS[:venus].add_supply(:peanut_butter, 50, 20)
@@ -43,6 +50,30 @@ PLANETS[:venus].add_supply(:mangos, 50, 20)
 PLANETS[:venus].add_demand(:bread, 100, 20)
 PLANETS[:venus].add_demand(:coffee, 100, 20)
 PLANETS[:venus].add_demand(:pasta, 100, 20)
+PLANETS[:venus].description = "You've arrived at Venus, a beutiful green ball of toxic rain and extreme pressure. Bring an umbrella."
+
+
+PLANETS[:saturn].add_supply(:star_juice, 100, 20)
+PLANETS[:saturn].add_supply(:peanut_butter, 70, 20)
+PLANETS[:saturn].add_supply(:mangos, 50, 20)
+PLANETS[:saturn].add_supply(:cheese, 50, 20)
+
+PLANETS[:saturn].add_demand(:bread, 110, 20)
+PLANETS[:saturn].add_demand(:cheese, 250, 20)
+PLANETS[:saturn].add_demand(:pickles, 400, 20)
+PLANETS[:saturn].add_demand(:purple_cow_ice_cream, 500, 20)
+PLANETS[:saturn].description = "The beautiful rings of Saturn greet you. Did you remember enough money to zoom home?"
+
+
+PLANETS[:jupiter].add_supply(:purple_cow_ice_cream, 100, 20)
+PLANETS[:jupiter].add_supply(:peanut_butter, 10, 20)
+PLANETS[:jupiter].add_supply(:mangos, 50, 20)
+
+PLANETS[:jupiter].add_demand(:coffee, 400, 20)
+PLANETS[:jupiter].add_demand(:mangos, 100, 20)
+PLANETS[:jupiter].add_demand(:pasta, 70, 20)
+PLANETS[:jupiter].description = "Jupiter largest plant in the solar system. Too bad your bank account isn't this large. Yet."
+
 
 #                    market: {
 #                      supply: {
